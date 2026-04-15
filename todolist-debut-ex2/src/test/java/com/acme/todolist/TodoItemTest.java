e65e258b8be67d8beb6ea00a8e2d6f026a20cccc
vagrant <mamadian14@gmail.com>
2026-04-15 16:26:51 +0200

    exo 3 terminé

---
+++ todolist-debut-ex2/src/test/java/com/acme/todolist/TodoItemTest.java
@@ -0,0 +1,38 @@
+package com.acme.todolist;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import com.acme.todolist.domain.TodoItem;
+import org.junit.jupiter.api.Test;
+
+class TodoItemTest {
+
+    @Test
+    void should_not_add_late_when_item_is_recent() {
+        TodoItem item = new TodoItem(
+                "1",
+                Instant.now(),
+                "Faire les courses"
+        );
+
+        String result = item.finalContent();
+
+        assertEquals("Faire les courses", result);
+    }
+
+    @Test
+    void should_add_late_when_item_is_older_than_24h() {
+        TodoItem item = new TodoItem(
+                "1",
+                Instant.now().minus(2, ChronoUnit.DAYS),
+                "Faire les courses"
+        );
+
+        String result = item.finalContent();
+
+        assertTrue(result.startsWith("[LATE!]"));
+    }
+}
